93eeddcd6c4a01d47036ed1f5c12eb85d910e613
Lovro <lovro.zskrlj@gmail.com>
2025-03-03 21:19:10 +0100

    začetek je začetek

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum CardNumber {
+    // označeno R kot rang karte, to sem si izmislil
+    // nevem če je to izraz
+    N2, N3, N4, N5, N6, N7,
+    N8, N9, N10, NJ, RQ, NK, NA    
+}
+enum CardColor {
+    Hearts, Spades, Diamonds, Clubs 
+}
+
+struct Card {
+    Color: CardColor,
+    Number: CardNumber
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
