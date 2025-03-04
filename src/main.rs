a1fbeb7d28b5b9aad2a2b2cd59bd038280e78e82
Lovro <lovro.zskrlj@gmail.com>
2025-03-04 22:54:57 +0100

    okno v katerem bo igrica se odpre

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate sdl2;
+
+use::sdl2::event::Event;
+use::sdl2::keyboard::Keycode;
+use::sdl2::pixels::Color;
+use::std::time::Duration;
+
+
 enum CardNumber {
     // označeno R kot rang karte, to sem si izmislil
     // nevem če je to izraz
@@ -9,11 +17,48 @@ enum CardColor {
 }
 
 struct Card {
-    Color: CardColor,
-    Number: CardNumber
+    color: CardColor,
+    number: CardNumber
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("POKEEEER", 800, 600)
+    .position_centered()
+    .opengl()
+    .build()
+    .unwrap();
+
+    let mut canvas = window
+    .into_canvas()
+    .build()
+    .unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter(){
+            match event {
+                | Event::Quit { .. }
+                | Event::KeyDown { 
+                    keycode: Some(Keycode::Escape),
+                    ..
+                 } => break 'running,
+                _ => {}
+            }
+        }
+
+        canvas.clear();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30))
+    }
+
+    
 }
