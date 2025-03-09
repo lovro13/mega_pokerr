b8fcc2e9a40690add3ce2d1e3ffd9eaed92a4bdb
Lovro <lovro.zskrlj@gmail.com>
2025-03-09 16:03:46 +0100

    naloži sliko karte

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-extern crate sdl2;
-
 use::sdl2::event::Event;
 use::sdl2::keyboard::Keycode;
 use::sdl2::pixels::Color;
+use sdl2::render::{WindowCanvas, Texture};
 use::std::time::Duration;
+use sdl2::image::{self, InitFlag, LoadTexture};
 
 
 enum CardNumber {
@@ -21,44 +21,58 @@ struct Card {
     number: CardNumber
 }
 
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+    canvas.set_draw_color(color);
+    canvas.clear();
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    canvas.copy(texture, None, None);
+
+    canvas.present();
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
     let window = video_subsystem.window("POKEEEER", 800, 600)
     .position_centered()
     .opengl()
     .build()
-    .unwrap();
+    .expect("could not initialize video subsystem");
 
     let mut canvas = window
     .into_canvas()
     .build()
-    .unwrap();
+    .expect("could not make canvas");
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.load_texture("assets/2_of_clubs.png")?;
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
+    canvas.clear();
+    canvas.present();
+    let mut i = 0;
     'running: loop {
         for event in event_pump.poll_iter(){
             match event {
                 | Event::Quit { .. }
-                | Event::KeyDown { 
-                    keycode: Some(Keycode::Escape),
-                    ..
-                 } => break 'running,
+                | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    break 'running;
+                },
                 _ => {}
             }
         }
+        
 
-        canvas.clear();
-        canvas.present();
+        i = (i + 1) & 255;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30))
     }
 
-    
+    Ok(())    
 }
