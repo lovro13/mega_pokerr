3730e3dbb8ce9656c5cf8ee472a1d29c70327e86
Lovro <lovro.zskrlj@gmail.com>
2025-03-09 18:57:08 +0100

    še bolj lepo pokaže karto na zaslonu

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use::sdl2::event::Event;
 use::sdl2::keyboard::Keycode;
 use::sdl2::pixels::Color;
-use sdl2::render::{WindowCanvas, Texture};
+use sdl2::{rect::{Point, Rect}, render::{Texture, WindowCanvas}, sys::CapButt};
 use::std::time::Duration;
 use sdl2::image::{self, InitFlag, LoadTexture};
 
+const CARD_HEIGHT: u32 = 100;
+const CARD_WIDTH: u32 = 80;
+const SCREEN_HEIGHT: u32 = 1000;
+const SCREEN_WIDTH: u32 = 1500;
 
 enum CardNumber {
     // označeno R kot rang karte, to sem si izmislil
@@ -21,11 +25,20 @@ struct Card {
     number: CardNumber
 }
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(canvas: &mut WindowCanvas, 
+    color: Color, 
+    texture: &Texture, 
+    position: Point
+) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
 
-    canvas.copy(texture, None, None);
+    let (width, height) = canvas.output_size()?;
+
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, CARD_WIDTH, CARD_HEIGHT);
+
+    canvas.copy(texture, None, screen_rect)?;
 
     canvas.present();
 
@@ -38,7 +51,7 @@ fn main() -> Result<(), String> {
 
     let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("POKEEEER", 800, 600)
+    let window = video_subsystem.window("POKEEEER", 1200, 600)
     .position_centered()
     .opengl()
     .build()
@@ -51,6 +64,7 @@ fn main() -> Result<(), String> {
 
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/2_of_clubs.png")?;
+    let position = Point::new(0, 0);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
@@ -69,8 +83,8 @@ fn main() -> Result<(), String> {
         }
         
 
-        i = (i + 1) & 255;
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+        i = (i + 1) % 255;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position)?;
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30))
     }
 
