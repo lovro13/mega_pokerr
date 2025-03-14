4614eacc0efbf9596f2875bfb84356ce07cdde51
Lovro <lovro.zskrlj@gmail.com>
2025-03-14 13:45:22 +0100

    zdaj so mi struct Player bolj usec, warning so zato ker se nisem uporabil nekaterih stvari iz enuma

--- src/card.rs
+++ src/card.rs
@@ -15,17 +15,17 @@ pub struct Card {
 }
 
 impl Card {
-    pub fn card_to_file(card: Card) -> String {
-        let card_color = card.color;
-        let card_num = card.number;
-        let string2 = match card_color {
+    pub fn card_to_file(card: &Card) -> String {
+
+        let Card { color, number } = card;
+        let string2 = match color {
             CardColor::Hearts => String::from("_of_hearts.png"),
             CardColor::Spades => String::from("_of_spades.png"),
             CardColor::Diamonds => String::from("_of_diamonds.png"),
             CardColor::Clubs => String::from("_of_clubs.png"),
-            CardColor::Empty => String::from("red_")
+            CardColor::Empty => String::from("")
         };
-        let string1 = match card_num  {
+        let string1 = match number  {
             CardNumber::N2 => String::from("2"),
             CardNumber::N3 => String::from("3"),
             CardNumber::N4 => String::from("4"),
@@ -39,11 +39,12 @@ impl Card {
             CardNumber::NQ => String::from("queen"),
             CardNumber::NK => String::from("king"),
             CardNumber::NA => String::from("ace"),
-            CardNumber::Empty => String::from("joker.png")
+            CardNumber::Empty => String::from("red_joker.png")
         };
         String::from("assets/") + &string1 + &string2
     }
 }
+
 
 pub enum Names {
     Player1, Player2, Player3, Player4, 
@@ -60,4 +61,54 @@ pub struct Player {
     pub card_position: (i32, i32),
     pub card_state: CardState
     // later to be finished
+}
+
+impl Names {
+    pub fn all_names() -> Vec<Names> {
+        vec![Names::Player1, Names::Player2, Names::Player3, Names::Player4, 
+        Names::Player5, Names::Player6, Names::Player7, Names::Player8]
+    } 
+}
+
+
+
+impl Player {
+    const PLAYER1_CARDS: (i32, i32) = (-775, 0);
+    const PLAYER2_CARDS: (i32, i32) = (-500, 275);
+    const PLAYER3_CARDS: (i32, i32) = (700, 0);
+    const PLAYER4_CARDS: (i32, i32) = (500, 275);
+    const PLAYER5_CARDS: (i32, i32) = (-50, 275);
+    const PLAYER6_CARDS: (i32, i32) = (-50, -300);
+    const PLAYER7_CARDS: (i32, i32) = (-500, -300);
+    const PLAYER8_CARDS: (i32, i32) = (500, -300);
+
+    pub fn init_players() -> Vec<Player> {
+        let mut list_of_players = Vec::new();
+        for name in Names::all_names() {
+            let curr_player = Player {
+                card_position: Self::get_card_position(&name),
+                name,
+                card: Card {
+                    color: CardColor::Empty,
+                    number: CardNumber::Empty
+                },
+                card_state: CardState::Opened
+            };
+            list_of_players.push(curr_player);
+        }
+        list_of_players
+    }
+
+    pub fn get_card_position(name: &Names) -> (i32, i32) {
+        match name {
+            Names::Player1 => Self::PLAYER1_CARDS,
+            Names::Player2 => Self::PLAYER2_CARDS,
+            Names::Player3 => Self::PLAYER3_CARDS,
+            Names::Player4 => Self::PLAYER4_CARDS,
+            Names::Player5 => Self::PLAYER5_CARDS,
+            Names::Player6 => Self::PLAYER6_CARDS,
+            Names::Player7 => Self::PLAYER7_CARDS,
+            Names::Player8 => Self::PLAYER8_CARDS,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use card::Player;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
-use std::io::Empty;
 use std::time::Duration;
 use sdl2::image::{self, InitFlag, LoadTexture};
 mod card;
@@ -14,90 +12,36 @@ const CARD_WIDTH: u32 = 95;
 const SCREEN_HEIGHT: u32 = 900;
 const SCREEN_WIDTH: u32 = 1800;
 
-const PLAYER1_CARDS: (i32, i32) = (-775, 0);
-const PLAYER2_CARDS: (i32, i32) = (-500, 275);
-const PLAYER3_CARDS: (i32, i32) = (700, 0);
-const PLAYER4_CARDS: (i32, i32) = (500, 275);
-const PLAYER5_CARDS: (i32, i32) = (-50, 275);
-const PLAYER6_CARDS: (i32, i32) = (-50, -300);
-const PLAYER7_CARDS: (i32, i32) = (-500, -300);
-const PLAYER8_CARDS: (i32, i32) = (500, -300);
-
-const EMPTY_CARD: card::Card = card::Card {
-    number: card::CardNumber::Empty,
-    color: card::CardColor::Empty
-};
-
-static mut PLAYER1: Player = Player {
-    name: card::Names::Player1,
-    card: EMPTY_CARD,
-    card_position: PLAYER1_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER2: Player = Player {
-    name: card::Names::Player2,
-    card: EMPTY_CARD,
-    card_position: PLAYER2_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER3: Player = Player {
-    name: card::Names::Player3,
-    card: EMPTY_CARD,
-    card_position: PLAYER3_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER4: Player = Player {
-    name: card::Names::Player4,
-    card: EMPTY_CARD,
-    card_position: PLAYER4_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER5: Player = Player {
-    name: card::Names::Player5,
-    card: EMPTY_CARD,
-    card_position: PLAYER5_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER6: Player = Player {
-    name: card::Names::Player6,
-    card: EMPTY_CARD,
-    card_position: PLAYER6_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER7: Player = Player {
-    name: card::Names::Player7,
-    card: EMPTY_CARD,
-    card_position: PLAYER8_CARDS,
-    card_state: card::CardState::Opened
-};
-static mut PLAYER8: Player = Player {
-    name: card::Names::Player8,
-    card: EMPTY_CARD,
-    card_position: PLAYER8_CARDS,
-    card_state: card::CardState::Opened
-};// to bo moral se drgac nrdit, pomoje da najbol v  main, pa da render uzame player_list
-// pa pol dela s temu, ka te se bojo povsod rabl
 
 fn render(canvas: &mut WindowCanvas, 
-    color: Color, 
-    texture: &Texture
+    background_color: Color, 
+    players_list: &Vec<card::Player>
 ) -> Result<(), String> {
-    let player_cards_position: Vec<(i32, i32)> = 
-    vec![PLAYER1_CARDS, PLAYER2_CARDS, PLAYER3_CARDS, PLAYER4_CARDS, 
-    PLAYER5_CARDS, PLAYER6_CARDS, PLAYER7_CARDS, PLAYER8_CARDS];
-    canvas.set_draw_color(color);
+
+    canvas.set_draw_color(background_color);
     canvas.clear();
 
     let (width, height) = canvas.output_size()?;
 
-    for position in player_cards_position {
+    for player in players_list {
+
+        
+        let texture_creator = canvas.texture_creator();
+        let filename = card::Card::card_to_file(&player.card);
+        
+        let texture = 
+        match player.card_state {
+            card::CardState::Closed => texture_creator.load_texture("assets/card_back.png")?,
+            card::CardState::Opened => texture_creator.load_texture(filename)?
+        };
+        let position = player.card_position;
         let screen_position = 
         Point::new(position.0, -position.1) + Point::new(width as i32 / 2, height as i32 / 2);
         let screen_rect_card1 = Rect::from_center(screen_position, CARD_WIDTH, CARD_HEIGHT);
         let screen_position2 = screen_position + Point::new(CARD_WIDTH as i32 - 30, 0);
         let screen_rect_card2 = Rect::from_center(screen_position2, CARD_WIDTH, CARD_HEIGHT);
-        canvas.copy(texture, None, screen_rect_card1)?;
-        canvas.copy(texture, None, screen_rect_card2)?;
+        canvas.copy(&texture, None, screen_rect_card1)?;
+        canvas.copy(&texture, None, screen_rect_card2)?;
     }
 
     canvas.present();
@@ -121,21 +65,13 @@ fn main() -> Result<(), String> {
     .into_canvas()
     .build()
     .expect("could not make canvas");
-
-    let texture_creator = canvas.texture_creator();
-    let card: card::Card = card::Card {
-        color: card::CardColor::Spades,
-        number: card::CardNumber::N2
-    };
-    let filename = card::Card::card_to_file(card);
-    let texture = texture_creator.load_texture(filename)?;
+    
     // let position = Point::new(PLAYER1_CARDS.0, PLAYER1_CARDS.1);
-
+    let mut player_list = card::Player::init_players();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     canvas.clear();
     canvas.present();
-    let mut i = 0;
     'running: loop {
         for event in event_pump.poll_iter(){
             match event {
@@ -148,7 +84,7 @@ fn main() -> Result<(), String> {
         }
         
 
-        render(&mut canvas, Color::RGB(200, 200, 255), &texture)?;
+        render(&mut canvas, Color::RGB(200, 200, 255), &player_list)?;
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30))
     }
 
